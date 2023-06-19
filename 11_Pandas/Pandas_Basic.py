3a5d0882016d9dfce57e2fc0c02280db304eac51
YeDongVibe <wkwhstladmss@naver.com>
2023-06-19 16:40:53 +0900

    23.06.19_Pandas

--- 11_Pandas/Pandas_Basic.py
+++ 11_Pandas/Pandas_Basic.py
@@ -513,7 +513,7 @@ print(df)
 
 #----------------------------------------------------------------------------------------------------------------------------
 # 데이터 형 변환 : apply():데이터프레임이나 시리즈에서 함수를 적용하여 새로운 결과를 반환하는 메소드
-df.apply(func, axis=0, **kwargs)
+# df.apply(func, axis=0, **kwargs)
 # func: 적용할 함수
 # axis: 적용할 축. 0은 열, 1은 행입니다. 기본값은 0입니다.
 # **kwargs: func 함수에 추가적으로 전달할 인수입니다.
@@ -563,6 +563,385 @@ df['words'] = df['text'].str.split()  # 공백을 기준으로 단어 분할
 df['no_punctuation'] = df['text'].str.replace('[^\w\s]', '', regex=True) # 구두점, 기호 제거 # [^\w\s] 패턴은 단어 문자와 공백 문자가 아닌 모든 문자
 df['word_count'] = df['text'].str.split().str.len()  # 단어 개수 계산
 
-print(df.iloc[:, 1:])
+print(df.iloc[:, 1:]) #1번 열부터 마지막 열까지
 
-#p.84
+#----------------------------------------------------------------------------------------------------------------------------
+#열 추가
+import pandas as pd
+
+# 데이터프레임 생성
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+        'age': [25, 30, 35, 40, 45],
+        'city': ['New York', 'Paris', 'London', 'Berlin', 'Tokyo']}
+df = pd.DataFrame(data)
+
+# 열 추가 방법 1: 기존 열을 이용하여 새로운 열 추가
+df['age2'] = df['age'] + 1
+
+# 열 추가 방법 2: insert() 메소드를 이용하여 특정 위치에 열 추가
+df.insert(loc=2, column='gender', value=['F', 'M', 'M', 'M', 'F'])
+
+# 열 추가 방법 3: assign() 메소드를 이용하여 여러 개의 열 한 번에 추가
+df = df.assign(age3=[26, 31, 36, 41, 46], height=[160, 170, 180, 175, 165])
+
+# 출력
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+#행 추가
+import pandas as pd
+
+# 데이터프레임 생성
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+        'age': [25, 30, 35, 40, 45],
+        'city': ['New York', 'Paris', 'London', 'Berlin', 'Tokyo']}
+df = pd.DataFrame(data)
+
+# 행 추가 방법 1: append() 메소드를 이용하여 단일 행 추가
+new_row = {'name': 'Frank', 'age': 50, 'city': 'Seoul'}
+df = df.append(new_row, ignore_index=True)
+
+# 행 추가 방법 2: append() 메소드를 이용하여 여러 행 추가
+new_rows = [{'name': 'George', 'age': 22, 'city': 'Toronto'},
+            {'name': 'Helen', 'age': 27, 'city': 'Sydney'}]
+df = df.append(new_rows, ignore_index=True)
+
+# 출력
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 삭제
+import pandas as pd
+
+# 데이터프레임 생성
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+        'age': [25, 30, 35, 40, 45],
+        'city': ['New York', 'Paris', 'London', 'Berlin', 'Tokyo']}
+df = pd.DataFrame(data)
+
+# 행 삭제 방법 1: drop() 메소드를 이용하여 단일 행 삭제
+df = df.drop(0)
+
+# 행 삭제 방법 2: drop() 메소드를 이용하여 여러 행 삭제
+df = df.drop([1, 2])
+
+# 열 삭제 방법 1: drop() 메소드를 이용하여 단일 열 삭제
+df = df.drop('age', axis=1)
+
+# 열 삭제 방법 2: drop() 메소드를 이용하여 여러 열 삭제
+df = df.drop(['name', 'city'], axis=1)
+
+# 출력
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+#재정렬
+import pandas as pd
+
+# 데이터프레임 생성
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+        'age': [25, 30, 35, 40, 45],
+        'city': ['New York', 'Paris', 'London', 'Berlin', 'Tokyo']}
+df = pd.DataFrame(data)
+
+# 행 재정렬 방법 1: loc[]을 이용하여 행 순서 변경
+df = df.loc[[4, 3, 2, 1, 0]]
+
+# 행 재정렬 방법 2: sort_values() 메소드를 이용하여 열 기준으로 정렬
+df = df.sort_values('age', ascending=False)
+
+# 열 재정렬 방법 1: 열 이름 순서 변경
+df = df[['city', 'name', 'age']]
+
+# 열 재정렬 방법 2: 열 이름을 알파벳 순서로 정렬
+df = df.reindex(sorted(df.columns), axis=1)
+
+# 출력
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+#Concat : 데이터프레임을 수직(위-아래) 또는 수평(좌-우)으로 연결하는 데 사용
+import pandas as pd
+
+df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+                    'B': ['B0', 'B1', 'B2', 'B3'],
+                    'C': ['C0', 'C1', 'C2', 'C3'],
+                    'D': ['D0', 'D1', 'D2', 'D3']})
+
+df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+                    'B': ['B4', 'B5', 'B6', 'B7'],
+                    'C': ['C4', 'C5', 'C6', 'C7'],
+                    'D': ['D4', 'D5', 'D6', 'D7']})
+
+result = pd.concat([df1, df2], axis=0, ignore_index=True)
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# Merge : 두 데이터프레임을 공통 열 또는 인덱스를 기준으로 병합하는 데 사용 <기본 조인 방법은 inner join>
+import pandas as pd
+
+left = pd.DataFrame({'key': ['K0', 'K1', 'K3'],
+                     'A': ['A0', 'A1', 'A3'],
+                     'B': ['B0', 'B1', 'B3']})
+
+right = pd.DataFrame({'key': ['K0', 'K2', 'K3'],
+                      'C': ['C0', 'C2', 'C3'],
+                      'D': ['D0', 'D2', 'D3']})
+
+result = pd.merge(left, right, on='key')
+
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# Join : 인덱스를 기준으로 두 데이터프레임을 병합하는 데 사용. <기본적으로 left join을 수행( how=＇inner'|'outer'|'right')> 
+import pandas as pd
+
+# 첫 번째 데이터프레임 생성
+data1 = {'이름': ['John', 'Steve', 'Sarah'],
+         '나이': [25, 32, 28],
+         '성별': ['남', '남', '여']}
+df1 = pd.DataFrame(data1)
+
+# 두 번째 데이터프레임 생성
+data2 = {'이름': ['Steve', 'Sarah', 'Mike'],
+         '키': [180, 163, 190],
+         '학년': ['2학년', '2학년', '3학년']}
+df2 = pd.DataFrame(data2)
+
+# 조인 작업 수행
+# how='inner': 내부 조인
+inner_join = df1.join(df2.set_index('이름'), on='이름', how='inner')
+print("Inner Join:")
+print(inner_join)
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# groupby() 함수 : 데이터프레임을 해당 열의 값에 따라 그룹화. 반환값은 그룹화된 데이터프레임을 나타내는 GroupBy 객체
+# grouped = df.groupby('열 이름')
+# grouped['열이름'].집계함수()
+
+# count(): 그룹별로 데이터의 개수를 계산합니다.
+# size(): 그룹별로 데이터의 크기를 계산합니다. count()와 다르게 NaN값도 포함합니다.
+# sum(): 그룹별로 데이터의 합을 계산합니다.
+# mean(): 그룹별로 데이터의 평균을 계산합니다.
+# median(): 그룹별로 데이터의 중앙값을 계산합니다.
+# min(): 그룹별로 데이터의 최솟값을 계산합니다.
+# max(): 그룹별로 데이터의 최댓값을 계산합니다.
+# std(): 그룹별로 데이터의 표준편차를 계산합니다.
+# var(): 그룹별로 데이터의 분산을 계산합니다.
+# sem(): 그룹별로 데이터의 표준오차를 계산합니다.
+# describe(): 그룹별로 기술통계값을 계산합니다.
+import pandas as pd
+
+
+Data= {'name': ['Alice', 'Bob', 'Charlie', 'David','Emily', 'Frank'],
+        'gender': ['F', 'M', 'M', 'M', 'F', 'M'],
+        'age': [25, 30, 35, 40, 45, 50],
+        'score1': [80, 70, 85, 75, 90, 95],
+        'score2': [85, 75, 90, 80, 95, 100]}
+
+df = pd.DataFrame(data)
+
+# count
+print(df.groupby('gender')[['score1', 'score2']].count())
+
+# size
+print(df.groupby('gender')[['score1', 'score2']].size())
+
+# sum
+print(df.groupby('gender')[['score1', 'score2']].sum())
+
+# mean
+print(df.groupby('gender')[['score1', 'score2']].mean())
+
+# median
+print(df.groupby('gender')[['score1', 'score2']].median())
+
+# min
+print(df.groupby('gender')[['score1', 'score2']].min())
+
+# max
+print(df.groupby('gender')[['score1', 'score2']].max())
+
+# '열 이름'은 그룹화된 데이터프레임에서 연산을 수행할 열의 이름
+# '통계 함수'에는 원하는 통계 함수(예: mean, sum, count, min, max 등)
+# result = grouped['열 이름'].agg(['통계 함수'])
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# agg 함수 : 다중 열에 대한 집계 함수를 적용하는 방법
+# grouped = df.groupby(['열이름A’, ‘열이름B’])
+# grouped.agg({'열1': '집계함수1', '열2': '집계함수2', ...})
+import pandas as pd
+
+df = pd.DataFrame({
+    'A': ['foo', 'bar', 'foo', 'bar', 'foo', 'bar', 'foo', 'foo'],
+    'B': ['one', 'one', 'two', 'three', 'two', 'two', 'one', 'three'],
+    'C': [1, 2, 3, 4, 5, 6, 7, 8],
+    'D': [10, 20, 30, 40, 50, 60, 70, 80]
+})
+# A와 B 컬럼에 대해서 그룹화한 결과에 대해 C 컬럼에 대해서는 count 함수를 적용하고, D 컬럼에 대해서는 sum과 mean 함수를 각각 적용
+
+result = df.groupby(['A', 'B']).agg({'C': 'count', 'D': ['sum', 'mean']})
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+
+df = pd.DataFrame({
+    'A': ['foo', 'bar', 'foo', 'bar', 'foo', 'bar', 'foo', 'foo'],
+    'B': ['one', 'one', 'two', 'three', 'two', 'two', 'one', 'three'],
+})
+
+def my_func(x):
+    return '-'.join(sorted(x))
+
+result = df.groupby('A').agg({'B': my_func})
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# Pivot_table : 데이터프레임에서 특정 열을 그룹화하여 행과 열을 피벗테이블 형태로 나타낼 수 있습니다.
+import pandas as pd
+
+df = pd.DataFrame({
+    'Name': ['Alice', 'Alice', 'Bob', 'Bob', 'Alice', 'Alice', 'Bob', 'Bob'],
+    'Date': ['Day 1', 'Day 2', 'Day 1', 'Day 2', 'Day 1', 'Day 2', 'Day 1', 'Day 2'],
+    'Value': [10, 20, 15, 25, 30, 40, 35, 45]
+})
+
+result = df.pivot_table(index='Name', columns='Date', values='Value', aggfunc='mean')
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+
+# 매출 데이터 생성
+data = {'Region': ['East', 'East', 'West', 'West', 'North', 'North', 'South', 'South'], 
+        'Time': ['Morning', 'Afternoon', 'Morning', 'Afternoon', 'Morning', 'Afternoon', 'Morning', 'Afternoon'],
+        'Sales': [100, 150, 200, 250, 300, 350, 400, 450]}
+df = pd.DataFrame(data)
+
+# 피벗테이블 생성
+result = df.pivot_table(index='Region', columns='Time', values='Sales', aggfunc='sum')
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+
+# 고객 데이터 생성
+data = {'Region': ['East', 'East', 'West', 'West', 'North', 'North', 'South', 'South'], 
+        'Age': [20, 30, 40, 50, 30, 40, 50, 60],
+        'Sales': [100, 150, 200, 250, 300, 350, 400, 450]}
+df = pd.DataFrame(data)
+
+# 피벗테이블 생성
+result = df.pivot_table(index='Region', columns=pd.cut(df['Age'], [10, 30, 50, 70]), values='Sales', aggfunc='mean')
+
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# cut 함수 : 주어진 데이터를 일정한 구간으로 나누어 범주형 데이터로 변환하는 함수. 생성된 카테고리는 groupby나 pivot_table과 같은 함수에서 인덱스 또는 열로 사용 가능.
+import pandas as pd
+
+# 나이 데이터 생성
+ages = pd.DataFrame({'age': [22, 44, 65, 86, 27, 19, 51, 92, 33, 35, 38, 42, 14, 50, 78]})
+
+# 연령대 구간 지정
+bins = [0, 20, 40, 60, 80, 100]
+
+# 연령대 카테고리 생성
+age_categories = pd.cut(ages['age'], bins)
+
+# 데이터프레임에 새로운 카테고리 열 추가
+ages['age_categories'] = age_categories
+
+# 결과 확인
+print(ages)
+result = pd.pivot_table(ages, index='age_categories', aggfunc='count')
+print(result)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 시계열 데이터 변환 : 문자열이나 다른 형식의 날짜와 시간 데이터를 datetime 형식으로 변환할 때 사용 <pd.to_datetime()>
+import pandas as pd
+
+data = {'date': ['2021-08-15', '2021-08-16', '2021-08-17'],
+        'value': [100, 200, 150]}
+df = pd.DataFrame(data)
+
+df['date'] = pd.to_datetime(df['date'])
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 문자열이 다른 형식으로 되어있을 경우 format 인자를 사용하여 문자열의 형식을 지정
+import pandas as pd
+
+# 날짜 데이터 생성
+df = pd.DataFrame({'date': ['2022년 01월 01일', '2022년 01월 02일', '2022년 01월 03일']})
+
+# 날짜 데이터를 datetime 형식으로 변환
+df['date'] = pd.to_datetime(df['date'], format='%Y년 %m월 %d일')
+
+# 년, 월, 일 컬럼 추출
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+
+print(df)
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# pd.resample() : 기존의 데이터셋을 새로운 시간 간격에 맞추어 변환해줍니다.
+import pandas as pd
+
+data = {'date': ['2022-01-01', '2022-01-02', '2022-01-01', '2022-01-02', '2022-01-01', '2022-01-02'],
+        'location': ['서울', '서울', '부산', '부산', '대구', '대구'],
+        'PM10': [50, 40, 45, 55, 60, 65],
+        'PM2.5': [25, 20, 22, 28, 30, 35]}
+df = pd.DataFrame(data)
+df['date'] = pd.to_datetime(df['date'])
+
+df_monthly = df.groupby('location').resample('D', on='date').mean(numeric_only=True)
+print(df_monthly)
+
+# 시계열 데이터 변환
+        # pd.resample()
+        # 시간 간격(alias) 문자열
+        # B: 평일(Business day) 기준으로 간격 지정
+        # C: 사용자 지정 시간대 내에서의 주기마다 간격 지정
+        # D: 달력 일(Day) 기준으로 간격 지정
+        # W: 주(Week) 기준으로 간격 지정
+        # M: 달(Month)의 마지막 날 기준으로 간격 지정
+        # SM: 반월(Semi-Month) 기준으로 간격 지정 (월의 15일과 말일을 기준)
+        # BM: 평일 기준으로 한 달의 마지막 날까지 간격 지정
+        # CBM: 사용자 지정 시간대 내에서 평일 기준으로 한 달의 마지막 날까지 간격 지정
+        # MS: 달(Month)의 시작일 기준으로 간격 지정
+        # SMS: 반월(Semi-Month) 기준으로 간격 지정 (월의 1일과 15일을 기준)
+        # BMS: 평일 기준으로 한 달의 시작일부터 마지막 날까지 간격 지정
+        # CBMS: 사용자 지정 시간대 내에서 평일 기준으로 한 달의 시작일부터 마지막 날까지 간격 지정
+        # Q: 분기(Quarter)의 마지막 날 기준으로 간격 지정
+        # BQ: 평일 기준으로 분기의 마지막 날까지 간격 지정
+        # QS: 분기(Quarter)의 시작일 기준으로 간격 지정
+        # BQS: 평일 기준으로 분기의 시작일부터 마지막 날까지 간격 지정
+        # A: 해(Year)의 마지막 날 기준으로 간격 지정
+        # BA: 평일 기준으로 해의 마지막 날까지 간격 지정
+        # AS: 해(Year)의 시작일 기준으로 간격 지정
+        # BAS: 평일 기준으로 해의 시작일부터 마지막 날까지 간격 지정
+
+
+# to_datetime에서 자동으로 인식하는 날짜 형식
+        # %Y-%m-%d %H:%M:%S.%f
+        # %Y-%m-%d %H:%M:%S
+        # %Y-%m-%d %H:%M
+        # %Y-%m-%d
+        # %m/%d/%Y %H:%M:%S
+        # %m/%d/%Y %H:%M
+        # %m/%d/%Y
+        # %m/%d/%y %H:%M:%S
+        # %m/%d/%y %H:%M
+        # %m/%d/%y
+
--- 11_Pandas/mission_Pandas.py
+++ 11_Pandas/mission_Pandas.py
@@ -27,3 +27,156 @@ print(ex4)
 print('-'*50)
 print(ex5)
 
+
+# 데이터 전처리 예제
+import pandas as pd
+
+data = {'이름': ['John', 'Steve', 'Sarah', 'Ann', 'Mike'],
+        '나이': [25, 32, 28, 35, 41],
+        '성별': ['남', '남', '여', '여', '남'],
+        '키': [175, 180, 163, 155, 190]}
+df = pd.DataFrame(data)
+
+# '학년' 열을 추가하고, 모든 행에 '3학년'이라는 값으로 채워 넣으세요.
+df['학년'] = '3학년'
+# '국적' 열을 추가하고, 각 행에 해당하는 사람의 국적을 채워 넣으세요.
+df['국적'] = ['한국', '일본', '미국', '중국', '네덜란드']
+# '성별'이 '여'인 행만 남기고 나머지 행을 삭제하세요.
+df = df[df['성별'] == '여']
+# '나이' 열을 기준으로 내림차순으로 행을 정렬하세요.
+df = df.sort_values(by='나이', ascending=False)
+# 인덱스가 2인 행을 삭제하세요.
+df = df.drop(2)
+# 아래 새로운 행 3개를 추가하세요.
+# [{'이름': 'Alex', '나이': 22, '성별': '남', '키': 180, '학년': '2학년', '국적': '미국'}, {'이름': 'Emily', '나이': 29, '성별': '여', '키': 165, '학년': '1학년', '국적': '캐나다'}, {'이름': 'Daniel', '나이': 33, '성별': '남', '키': 175, '학년': '3학년', '국적': '호주'}]
+new_rows = [{'이름': 'Alex', '나이': 22, '성별': '남', '키': 180, '학년': '2학년', '국적': '미국'}, {'이름': 'Emily', '나이': 29, '성별': '여', '키': 165, '학년': '1학년', '국적': '캐나다'}, {'이름': 'Daniel', '나이': 33, '성별': '남', '키': 175, '학년': '3학년', '국적': '호주'}]
+
+df = df.append(new_rows, ignore_index = True)
+
+# '이름' 열을 기준으로 오름차순으로 행을 정렬하세요.
+df = df.sort_values(by='이름', ascending=True)
+# '키' 열의 값을 cm에서 m로 변환하세요.
+df['키'] = df['키']/100
+# '성별' 열을 삭제하세요.
+df = df.drop('성별', axis=1)
+
+print(df)
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = {'Name': ['John', 'Mike', 'Sarah', 'Adam', 'Emily', 'Daniel', 'Olivia', 'Liam', 'Sophia', 'Ethan',
+                 'Emma', 'Jacob', 'Ava', 'Mia', 'Noah', 'Charlotte', 'Harper', 'William', 'Benjamin', 'Elijah',
+                 'Amelia', 'James', 'Oliver', 'Lucas', 'Mason', 'Logan', 'Alexander', 'Evelyn', 'Grace', 'Victoria'],
+        'Age': np.random.randint(20, 40, 30),
+        'Gender': ['Male', 'Male', 'Female', 'Male', 'Female', 'Male', 'Female', 'Male', 'Female', 'Male',
+                   'Female', 'Male', 'Female', 'Female', 'Male', 'Female', 'Female', 'Male', 'Male', 'Male',
+                   'Female', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male', 'Female', 'Female', 'Female'],
+        'City': ['New York', 'Paris', 'London', 'Sydney', 'Tokyo', 'Berlin', 'Rome', 'Madrid', 'Seoul', 'Beijing',
+                 'Moscow', 'Vienna', 'Athens', 'Cairo', 'Lisbon', 'Dublin', 'Amsterdam', 'Stockholm', 'Oslo', 'Helsinki',
+                 'Copenhagen', 'Budapest', 'Prague', 'Warsaw', 'Vienna', 'Brussels', 'Luxembourg', 'Zurich', 'Geneva', 'Dubai']}
+df = pd.DataFrame(data)
+
+df['initial'] = df['Name'].str[0]
+mean_age_by_initial = df.groupby('initial')['Age'].mean()
+print(mean_age_by_initial)
+
+overall_mean_age = df['Age'].mean()
+
+
+plt.bar(mean_age_by_initial.index, mean_age_by_initial.values)
+plt.axhline(overall_mean_age, color = 'red', linestyle = '--', label = 'overall_mean_age') #수평선 긋기
+
+plt.xlabel('initial')
+plt.ylabel('Mean Age')
+plt.legend()
+plt.show()
+
+#----------------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# 데이터 파일 경로
+data_path = 'C:/Ye_Dong/Python_Class/11_Pandas/일별평균대기오염도_2022.csv'
+
+# CSV 파일 읽기
+df = pd.read_csv(data_path, encoding="cp949")
+
+# 필요한 필드 추출
+df = df[['측정일시', '측정소명', '미세먼지농도(㎍/㎥)', '초미세먼지농도(㎍/㎥)']]
+
+# 결측치 처리
+df = df.dropna()
+
+# 측정일시 컬럼의 데이터 타입을 datetime으로 변경
+df['측정일시'] = pd.to_datetime(df['측정일시'], format='%Y%m%d')
+
+print(df.head(20))
+
+#----------------------------------------------------------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+# 연도별 미세먼지와 초미세먼지 농도 평균 계산
+df_monthly = df.resample('M', on='측정일시').mean(numeric_only=True)
+
+# 그래프 그리기
+plt.plot(df_monthly.index.month, df_monthly['미세먼지농도(㎍/㎥)'], label='PM10')
+plt.plot(df_monthly.index.month, df_monthly['초미세먼지농도(㎍/㎥)'], label='PM2.5')
+plt.legend()
+plt.xlabel('Month')
+plt.ylabel('Concentration')
+plt.title('2022 Air Pollution Trend')
+plt.show()
+
+#----------------------------------------------------------------------------------------------------------------------------
+#일별 대기오염 추이 그래프
+import matplotlib.pyplot as plt
+
+# 일별 합계 계산
+df_daily = df.resample('D', on='측정일시').sum(numeric_only=True)
+
+# 일평균 대기오염도 계산
+df_daily['미세먼지농도(㎍/㎥)'] /= 24
+df_daily['초미세먼지농도(㎍/㎥)'] /= 24
+
+# 그래프 그리기
+plt.plot(df_daily.index, df_daily['미세먼지농도(㎍/㎥)'], label='PM10')
+plt.plot(df_daily.index, df_daily['초미세먼지농도(㎍/㎥)'], label='PM2.5')
+plt.legend()
+plt.xlabel('Date')
+plt.ylabel('Concentration')
+plt.title('2022 Air Pollution Trend')
+plt.show()
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 지역별 대기오염 막대 그래프
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'
+
+# 지역별 미세먼지와 초미세먼지 농도 평균 계산
+df_area = df.groupby('측정소명').mean(numeric_only=True).head(20)
+
+# 그래프 그리기
+plt.bar(df_area.index, df_area['미세먼지농도(㎍/㎥)'], label='PM10')
+plt.bar(df_area.index, df_area['초미세먼지농도(㎍/㎥)'], label='PM2.5')
+plt.legend()
+plt.xlabel('Area')
+plt.ylabel('Concentration')
+plt.title('Air Pollution by Area')
+plt.show()
+
+#----------------------------------------------------------------------------------------------------------------------------
+# 대기오염 상자그림
+import matplotlib.pyplot as plt
+
+# 그래프 그리기
+plt.boxplot([df['미세먼지농도(㎍/㎥)'], df['초미세먼지농도(㎍/㎥)']])
+plt.xticks([1,2],['PM10', 'PM2.5'])
+plt.ylabel('Concentration')
+plt.title('Air Pollution Boxplot')
+plt.show()
